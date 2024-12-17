341a2b22f1e81dd3fe81d25823cb8ee75fb1f930
saltydoge59 <curteisyang59@gmail.com>
2024-12-17 13:05:36 +0800

    chnaged upload color

--- src/components/ui/file-upload.tsx
+++ src/components/ui/file-upload.tsx
@@ -70,10 +70,10 @@ export const FileUpload = ({
           <GridPattern />
         </div>
         <div className="flex flex-col items-center justify-center">
-          <p className="relative z-20 font-sans font-bold text-neutral-700 dark:text-neutral-300 text-base">
+          <p className="relative z-20 font-sans font-bold text-base">
             Upload file
           </p>
-          <p className="relative z-20 font-sans font-normal text-neutral-400 dark:text-neutral-400 text-base mt-2">
+          <p className="relative z-20 font-sans font-normal text-base mt-2">
             Drag or drop your files here or click to upload
           </p>
           <div className="relative w-full mt-10 max-w-xl mx-auto">
